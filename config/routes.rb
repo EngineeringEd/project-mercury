5bd97676cd87e5e4b3c2d5edff8f3fab9a4e9286
edwardheaver <ed.heaver@justpark.com>
2022-08-19 12:28:03 +0100

    Getting ready for deplopy

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'welcome#index'
 
   namespace :api, constraint: { format: 'json' } do
     namespace :v1 do
